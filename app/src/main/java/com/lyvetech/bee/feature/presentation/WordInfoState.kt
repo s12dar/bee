716c202c5f94b6c8aaa4b6b319e98fc6f5f0358f
pserdax <parahatgeldiyew.s@gmail.com>
2022-02-19 23:29:38 +0200

    MVVM implementation

---
+++ app/src/main/java/com/lyvetech/bee/feature/presentation/WordInfoState.kt
@@ -0,0 +1,10 @@
+package com.lyvetech.bee.feature.presentation
+
+import com.lyvetech.bee.feature.domain.model.WordInfo
+
+data class WordInfoState(
+    val wordInfoItems: List<WordInfo> = emptyList(),
+    val isLoading: Boolean = false
+) {
+
+}
---
+++ app/src/main/java/com/lyvetech/bee/feature/presentation/WordInfoViewModel.kt
@@ -0,0 +1,73 @@
+package com.lyvetech.bee.feature.presentation
+
+import androidx.compose.runtime.State
+import androidx.compose.runtime.mutableStateOf
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.lyvetech.bee.core.util.Resource
+import com.lyvetech.bee.feature.domain.usecases.GetWordInfo
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.flow.MutableSharedFlow
+import kotlinx.coroutines.flow.asSharedFlow
+import kotlinx.coroutines.flow.launchIn
+import kotlinx.coroutines.flow.onEach
+import kotlinx.coroutines.launch
+import javax.inject.Inject
+
+@HiltViewModel
+class WordInfoViewModel @Inject constructor(
+    private val getWordInfo: GetWordInfo
+) : ViewModel() {
+    private val _searchQuery = mutableStateOf("")
+    val searchQueury: State<String> = _searchQuery
+
+    private val _wordInfoState = mutableStateOf(WordInfoState())
+    private val wordInfoState: State<WordInfoState> = _wordInfoState
+
+    private val _eventFlow = MutableSharedFlow<UIEvent>()
+    val eventFlow = _eventFlow.asSharedFlow()
+
+    private var searchJob: Job? = null
+
+    fun onSearch(query: String) {
+        _searchQuery.value = query
+        searchJob?.cancel()
+        searchJob = viewModelScope.launch {
+            delay(500L)
+            getWordInfo(query)
+                .onEach { result ->
+                    when (result) {
+                        is Resource.Success -> {
+                            _wordInfoState.value = wordInfoState.value.copy(
+                                wordInfoItems = result.data ?: emptyList(),
+                                isLoading = false
+                            )
+                        }
+                        is Resource.Error -> {
+                            _wordInfoState.value = wordInfoState.value.copy(
+                                wordInfoItems = result.data ?: emptyList(),
+                                isLoading = false
+                            )
+                            _eventFlow.emit(
+                                UIEvent.ShowSnackBar(
+                                    result.message ?: "Unknown error"
+                                )
+                            )
+                        }
+                        is Resource.Loading -> {
+                            _wordInfoState.value = wordInfoState.value.copy(
+                                wordInfoItems = result.data ?: emptyList(),
+                                isLoading = true
+                            )
+                        }
+                    }
+                }.launchIn(this)
+        }
+    }
+
+    sealed class UIEvent {
+        data class ShowSnackBar(val message: String) : UIEvent()
+    }
+}
